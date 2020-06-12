35d103d8fbf6296b0ab48327ea693804ce3eb2cc
z5210209 <z5210209@cse.unsw.edu.au>
2020-06-12 15:19:38 +1000

    Actual Sneaky echon.sh pass all autotests with no loop

--- lab02/echon.sh
+++ lab02/echon.sh
@@ -4,7 +4,7 @@ if [ $# -ne 2 ]
 then
     echo "Usage: ./echon.sh <number of lines> <string>"
     exit 1
-elif [ $1 -lt 0 ]
+elif ! test $1 -ge 0 2>/dev/null
 then
     echo "./echon.sh: argument 1 must be a non-negative integer"
     exit 1
