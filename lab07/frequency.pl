337804d503de8de321d6283e4f90a178fe919962
z5210209 <z5210209@cse.unsw.edu.au>
2020-07-21 11:48:40 +1000

    Finish lab07 frequency.pl

--- lab07/frequency.pl
+++ lab07/frequency.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 
-
-foreach $lyr_file (glob "lyrics/*.txt") {
-    $artist = $lyr_file;
+$word = $ARGV[0];
+foreach $file (glob "lyrics/*.txt") {
+    
+    # Extract Artist's Name
+    $artist = $file;
     # replace .txt with nothing
     $artist =~ s/\.txt$//;  
     # replace anything before the first '/' which is inherently "lyrics"  
@@ -10,18 +12,56 @@ foreach $lyr_file (glob "lyrics/*.txt") {
     # replace '_' with ' '
     $artist =~ s/_/ /g;
     
-    # print "$artist\n"
-    open $file, '<', $lyr_file
-    while ($line = <$file>) {
+    # total_words in file
+    $total = total_words($file);
     
+    # count_words occurence
+    $word_count = count_occurence($file, $word);
+    
+    # Calculate Frequency
+    $frequency = $word_count/$total;
+    
+    # Result
+    printf ("%4d/%6d = %.9f %s\n", $word_count, $total, $frequency, $artist);
+}
+
+sub total_words {
+    
+    my ($file) = @_;
+    open (FH, '<', $file) or die $!;
+    
+    $count = 0;
+    while ($line = <FH>) {
+        @words = split /[^a-zA-Z]+/, $line;
+        foreach $word (@words) {
+            $count++ if $word ne '';
+        }
+    }
+    
+    close (FH);
+    return $count;
+}
+
+
+sub count_occurence {
+    my ($file, $search_word) = @_;
+    $search_word = lc $search_word;
+    
+    open (FH, '<', $file) or die $!;
+    
+    $count = 0;
+    while ($line = <FH>) {
         $line = lc $line;
         @words = split /[^a-z]+/, $line;
-        
         foreach $word (@words) {
             $count++ if $word eq $search_word;
         }
     }
     
+    close (FH);
+    return $count;
 }
+
+
 
 
